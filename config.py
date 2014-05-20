0f14bcced80a2e5ba6815b1a576bb712b16c73f8
caryoscelus <caryoscelus@gmx.com>
2014-05-21 01:30:45 +0400

    use defaults for CA_HOST & CA_SITE_ID

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ def get_configjs():
     # plain config.js (example of it can be found in config.js.example)
     #return open('config.js', 'r').read()
     
-    host = os.environ['CA_HOST']
-    site_id = os.environ['CA_SITE_ID']
+    host = os.environ.get('CA_HOST', 'http://localhost:5000/')
+    site_id = os.environ.get('CA_SITE_ID', 'test')
     return 'var rest_server = "{0}";\nvar site_id="{1}"'.format(host, site_id)
